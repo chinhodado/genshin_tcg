5145ab876af9114a5646854ec11d212ca9c768c4
Trung Do <chin.bimbo@gmail.com>
2022-11-13 01:46:48 -0500

    Some initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main-container">
+      <div id="left-panel">somethging</div>
+      <div id="right-panel">
+        <div id="top-field"></div>
+        <div id="bottom-field">
+          <div id="active-char-1"></div>
+          <div id="bench1-char-1"></div>
+          <div id="bench2-char-1"></div>
+          <div id="dice-1">
+            <table id="dice-table-1">
+              <tr>
+                <td><div id="dice1-1" className="dice"></div></td>
+                <td><div id="dice2-1" className="dice"></div></td>
+                <td><div id="dice3-1" className="dice"></div></td>
+                <td><div id="dice4-1" className="dice"></div></td>
+              </tr>
+              <tr>
+                <td><div id="dice5-1" className="dice"></div></td>
+                <td><div id="dice6-1" className="dice"></div></td>
+                <td><div id="dice7-1" className="dice"></div></td>
+                <td><div id="dice8-1" className="dice"></div></td>
+              </tr>
+            </table>
+          </div>
+          <div id="deck-1">Deck</div>
+        </div>
+      </div>
     </div>
   );
 }
